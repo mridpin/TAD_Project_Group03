890bcabc1a113d95c55fcf21c013931b88ed4b9d
nonorr2 <nono.rr2@gmail.com>
2018-04-18 02:48:10 +0200

    Creacion de la pantalla login

---
+++ TournamentManager/src/main/java/upo/tad/tournamentmanager/view/LoginScreen.java
@@ -0,0 +1,118 @@
+package upo.tad.tournamentmanager.view;
+
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.Page;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+import upo.tad.tournamentmanager.controller.PlayerController;
+
+public class LoginScreen extends CssLayout {
+
+    PlayerController pc = new PlayerController();
+
+    private TextField username;
+    private PasswordField password;
+    private Button login;
+
+    public LoginScreen() {
+        buildUI();
+        username.focus();
+    }
+
+    //Crea pantalla
+    private void buildUI() {
+        addStyleName("login-screen");
+
+        // login form, centered in the available part of the screen
+        Component loginForm = buildLoginForm();
+
+        // layout to center login form when there is sufficient screen space
+        // - see the theme for how this is made responsive for various screen
+        // sizes
+        VerticalLayout centeringLayout = new VerticalLayout();
+        centeringLayout.setStyleName("centering-layout");
+        centeringLayout.addComponent(loginForm);
+        centeringLayout.setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
+
+        // information text about logging in
+        CssLayout loginInformation = buildLoginInformation();
+
+        addComponent(centeringLayout);
+        addComponent(loginInformation);
+    }
+
+    //Ventana de en medio para login
+    private Component buildLoginForm() {
+        FormLayout loginForm = new FormLayout();
+
+        loginForm.addStyleName("login-form");
+        loginForm.setSizeUndefined();
+        loginForm.setMargin(false);
+
+        loginForm.addComponent(username = new TextField("Username"));
+        username.setWidth(15, Unit.EM);
+        loginForm.addComponent(password = new PasswordField("Password"));
+        password.setWidth(15, Unit.EM);
+        password.setDescription("Enter your password");
+        CssLayout buttons = new CssLayout();
+        buttons.setStyleName("buttons");
+        loginForm.addComponent(buttons);
+
+        buttons.addComponent(login = new Button("Login"));
+        login.setDisableOnClick(true);
+
+        //Cuando hace click en login
+        login.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                try {
+                    login();
+                } finally {
+                    login.setEnabled(true);
+                }
+            }
+        });
+        login.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+        login.addStyleName(ValoTheme.BUTTON_FRIENDLY);
+        return loginForm;
+    }
+
+    //Layaout lateral con informacion
+    private CssLayout buildLoginInformation() {
+        CssLayout loginInformation = new CssLayout();
+        loginInformation.setStyleName("login-information");
+        Label loginInfoText = new Label("<h1>Warhammer 4K</h1>", ContentMode.HTML);
+        loginInformation.addComponent(loginInfoText);
+        return loginInformation;
+    }
+
+    //Hace en login cunado se le da al boton
+    private void login() {
+//        boolean pass = pc.checkLogin(username.getValue(), password.getValue());
+//        if (pass) {
+//            loginListener.loginSuccessful();
+//        } else {
+//            showNotification(new Notification("Login failed", "Please check your username and password and try again.",
+//                    Notification.Type.HUMANIZED_MESSAGE));
+//            username.focus();
+//        }
+    }
+
+    //Muestra las notificaciones pasadas como parametro
+    private void showNotification(Notification notification) {
+        // keep the notification visible a little while after moving the
+        // mouse, or until clicked
+        notification.setDelayMsec(2000);
+        notification.show(Page.getCurrent());
+    }
+}
