4a0194d84abdffb749220a737cd5588f0793227c
Antonio Jota <antoniojherrera@MacBook-Air-de-Antonio.local>
2018-04-19 19:03:54 +0200

    Armies view
    Terminada la vista de armies

--- TournamentManager/src/main/java/upo/tad/tournamentmanager/view/panels/ArmiesPanel.java
+++ TournamentManager/src/main/java/upo/tad/tournamentmanager/view/panels/ArmiesPanel.java
@@ -3,45 +3,164 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package upo.tad.tournamentmanager.view.panels;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.Select;
 import com.vaadin.ui.Table;
-
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
- * 
- * @author Antonio Jose Herrera Tabaco 
+ *
+ * @author Antonio Jose Herrera Tabaco
  */
-public class ArmiesPanel extends CssLayout implements View{
+public class ArmiesPanel extends CssLayout implements View {
 
-    public ArmiesPanel(){
-        
+    public ArmiesPanel() {
+
         setSizeFull();
         addStyleName("armies");
+
+        HorizontalSplitPanel mainLayout = new HorizontalSplitPanel();
+        mainLayout.setLocked(true);
+        mainLayout.setSplitPosition(80, Unit.PERCENTAGE);
+
+        VerticalLayout left = new VerticalLayout();
+        left.setSpacing(true);
+        left.setMargin(true);
+        left.setSizeFull();
+
+        VerticalLayout right = new VerticalLayout();
+        right.setSpacing(true);
+        right.setMargin(true);
+
+        mainLayout.setFirstComponent(left);
+        mainLayout.setSecondComponent(right);
+
+        addComponent(mainLayout);
 
         Table table = new Table();
 
         table.addContainerProperty("Name", String.class, null);
         table.addContainerProperty("Faction", String.class, null);
         table.addContainerProperty("Strategy", String.class, null);
-        table.addContainerProperty("Nickname", String.class, null);        
+        table.addContainerProperty("Nickname", String.class, null);
 
-        table.addItem(new Object[]{"Army 1", "Faction 1", "Distancia", "Player 1"}, null);
-        table.addItem(new Object[]{"Army 2", "Faction 2", "Cuerpo a cuerpo", "Player 1"}, null);
-        table.addItem(new Object[]{"Army 3", "Faction 1", "Distancia", "Player 2"}, null);
-        table.addItem(new Object[]{"Army 4", "Faction 3", "Cuerpo a cuerpo", "Player 3"}, null);
+        rellenaTabla(table);
+
+        table.setWidth(100, Unit.PERCENTAGE);
+        table.setHeight(100, Unit.PERCENTAGE);
+        table.setSelectable(true);
+
+        left.addComponent(table);
+
+        TextField army_name = new TextField("Name");
+        army_name.setIcon(FontAwesome.USER);
+        army_name.setWidth(100, Unit.PERCENTAGE);
+        ComboBox army_faction = new ComboBox("Faction");
+        army_faction.addItem("Caos");
+        army_faction.addItem("Aeldari");
+        army_faction.setNullSelectionAllowed(false);
+        army_faction.setIcon(FontAwesome.GAMEPAD);
+        army_faction.setWidth(100, Unit.PERCENTAGE);  
+        /*
+        get faction
+        army_faction.addValueChangeListener(event
+                -> // Java 8
+                layout.addComponent(new Label("Selected "
+                        + event.getProperty().getValue()))
+        );
+        */
+      
+        ComboBox army_strategy = new ComboBox("Strategy");
+        army_strategy.addItem("A distancia");
+        army_strategy.addItem("Cuerpo a cuerpo");
+        army_strategy.setNullSelectionAllowed(false);
+        army_strategy.setIcon(FontAwesome.GAMEPAD);
+        army_strategy.setWidth(100, Unit.PERCENTAGE);
+
+        Button create = new Button("Create");
+        create.setIcon(FontAwesome.PLUS);
+        create.setWidth(100, Unit.PERCENTAGE);
+        create.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+        Button update = new Button("Update");
+        update.setIcon(FontAwesome.ARROW_UP);
+        update.setWidth(100, Unit.PERCENTAGE);
+        update.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+        update.setVisible(false);
+        Button clean = new Button("Clean");
+        clean.setIcon(FontAwesome.REFRESH);
+        clean.setWidth(100, Unit.PERCENTAGE);
+        clean.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        Button remove = new Button("Remove");
+        remove.setIcon(FontAwesome.REMOVE);
+        remove.setWidth(100, Unit.PERCENTAGE);
+        remove.setStyleName(ValoTheme.BUTTON_DANGER);
+
+        right.addComponents(army_name, army_faction, army_strategy, create, update, clean, remove);
         
-        addComponents(table);
+        table.addItemClickListener((event) -> {
+            Object currentItemId = event.getItemId();
+            String name = (String) table.getItem(currentItemId).getItemProperty("Name").getValue();
+            String faction = (String) table.getItem(currentItemId).getItemProperty("Faction").getValue();
+            String strategy = (String) table.getItem(currentItemId).getItemProperty("Strategy").getValue();
+
+            army_name.setValue(name);
+
+            
+            update.setVisible(true);
+            create.setVisible(false);
+        });
+        
+        create.addClickListener((event) -> {
+            //CREAR EJERCITO
+            rellenaTabla(table);
+            army_name.clear();            
+        });
+        
+        clean.addClickListener((event) -> {
+            army_name.clear();
+            create.setVisible(true);
+            update.setVisible(false);
+        });
+        
+        remove.addClickListener((event) -> {
+            //BORRAR JUGADOR
+            rellenaTabla(table);
+            army_name.clear();
+            create.setVisible(true);
+            update.setVisible(false);
+        });
+        
+        update.addClickListener((event) -> {
+            //ACTUALIZAR JUGADOR
+            rellenaTabla(table);
+            army_name.clear();
+            create.setVisible(true);
+            update.setVisible(false);
+        });
+
     }
 
     @Override
     public void enter(ViewChangeEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
-       
+    private void rellenaTabla(Table table) {
+        table.addItem(new Object[]{"Army 1", "Faction 1", "Distancia", "Player 1"}, null);
+        table.addItem(new Object[]{"Army 2", "Faction 2", "Cuerpo a cuerpo", "Player 1"}, null);
+        table.addItem(new Object[]{"Army 3", "Faction 1", "Distancia", "Player 2"}, null);
+        table.addItem(new Object[]{"Army 4", "Faction 3", "Cuerpo a cuerpo", "Player 3"}, null);
+    }
+
 }
