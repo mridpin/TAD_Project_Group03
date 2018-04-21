4b630e26c4e0267eb4525171b230c21e3749f1e4
mridpin <mridao99@gmail.com>
2018-04-21 20:53:36 +0200

    wip los graficos de rankings

--- TournamentManager/src/main/java/upo/tad/tournamentmanager/view/panels/RankingsPanel.java
+++ TournamentManager/src/main/java/upo/tad/tournamentmanager/view/panels/RankingsPanel.java
@@ -7,6 +7,13 @@ package upo.tad.tournamentmanager.view.panels;
 
 import POJOs.Army;
 import POJOs.Player;
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.DataSeries;
+import com.vaadin.addon.charts.model.PlotOptionsPie;
+import com.vaadin.addon.charts.model.style.SolidColor;
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.CssLayout;
@@ -40,6 +47,21 @@ public class RankingsPanel extends CssLayout implements View {
         addStyleName("rankings");
         this.loadData();
 
+        /*Right side: charts*/
+        VerticalSplitPanel right = new VerticalSplitPanel();
+        // Chart 1: Win/loss count
+        Chart pie = new Chart(ChartType.PIE);
+        pie.setWidth(100, Unit.PERCENTAGE);
+        pie.setHeight(100, Unit.PERCENTAGE);
+        Configuration conf = pie.getConfiguration();
+        conf.setTitle("Performance");
+        conf.setSubTitle("Wins vs. Loses");
+        DataSeries data1 = new DataSeries();
+        PlotOptionsPie plot = new PlotOptionsPie();
+        plot.setColors(new SolidColor[]{new SolidColor("green"), new SolidColor("red")});
+        data1.setPlotOptions(plot);
+        conf.addSeries(data1);
+        right.setFirstComponent(pie);
         /* Left side: Tables */
         // Divide left side in 3 equal parts
         VerticalSplitPanel leftMidBot = new VerticalSplitPanel();
@@ -60,6 +82,7 @@ public class RankingsPanel extends CssLayout implements View {
         }
         playersTable.setWidth(100, Unit.PERCENTAGE);
         playersTable.setPageLength(0);
+        playersTable.setSelectable(true);
         left.setFirstComponent(playersTable);
 
         // Table 2: Armies
@@ -78,8 +101,19 @@ public class RankingsPanel extends CssLayout implements View {
         }
         armiesTable.setWidth(100, Unit.PERCENTAGE);
         armiesTable.setPageLength(0);
+        armiesTable.setSelectable(true);
+        armiesTable.addItemClickListener(new ItemClickEvent.ItemClickListener() {
+            @Override
+            /**
+             * Changes the information displayed on the graphs according to what
+             * has been clicked
+             */
+            public void itemClick(ItemClickEvent event) {
+                data1.clear();
+            }
+        });
         leftMidBot.setFirstComponent(armiesTable);
-        
+
         // Table 3: Factions ranking
         Table factionsTable = new Table("Factions Rankings");
         factionsTable.addContainerProperty("Faction Name", String.class, null);
@@ -96,9 +130,9 @@ public class RankingsPanel extends CssLayout implements View {
         }
         factionsTable.setWidth(100, Unit.PERCENTAGE);
         factionsTable.setPageLength(0);
+        factionsTable.setSelectable(true);
         leftMidBot.setSecondComponent(factionsTable);
-        
-        VerticalSplitPanel right = new VerticalSplitPanel();
+
         HorizontalSplitPanel hsp = new HorizontalSplitPanel(left, right);
         addComponent(hsp);
     }
