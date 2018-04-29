00f6bc749e77d606ac765966b94c13cff49e9894
Antonio Jota <antoniojherrera@localhost>
2018-04-29 12:56:30 +0200

    Javadoc para los metodos que quedaban de PlayerController

--- TournamentManager/src/main/java/upo/tad/tournamentmanager/controller/PlayerController.java
+++ TournamentManager/src/main/java/upo/tad/tournamentmanager/controller/PlayerController.java
@@ -21,19 +21,47 @@ public class PlayerController {
 
     DAO dao = new DAO();
 
+    /**
+     * Check if a player can log in the app by his username and password
+     * 
+     * @param username Nickname of the player
+     * @param password Password of the player
+     * @return A entity player
+     */
     public Player checkLogin(String username, String password) {
         return dao.login(username, password);
     }
 
+    /**
+     * Get a list of all players
+     * 
+     * @return List of all players
+     */
     public List<Player> getPlayers() {
         return dao.getPlayers();
     }
 
+    /**
+     * Add a new player 
+     * 
+     * @param name Name of the player
+     * @param nickName Nickname of the player
+     * @param password Password of the player
+     * @param email Email of the player
+     */
     public void addPlayer(String name, String nickName, String password, String email) {
         Player p = new Player(name, password, email, 0, nickName, true);
         dao.addPlayer(p);
     }
 
+    /**
+     * Modifies the data that the player has changed
+     * 
+     * @param name New name of the player
+     * @param nickName New nickname of the player
+     * @param password New password of the player
+     * @param email New email of the player
+     */
     public void updatePlayer(String name, String nickName, String password, String email) {
         Player p = dao.getPlayer(nickName);
         p.setName(name);
@@ -45,11 +73,26 @@ public class PlayerController {
         dao.updatePlayer(p);
     }
 
+    /**
+     * Remove a player from BD
+     * 
+     * @param nickName Nickname of the player
+     */
     public void removePlayer(String nickName) {
         Player p = dao.getPlayer(nickName);
         dao.removePlayer(p);
     }
 
+    /**
+     * Update the profile of the own player
+     * 
+     * @param name New name of the player
+     * @param nickname New nickname of the player
+     * @param email New email of the player
+     * @param old_pass Old password of the player
+     * @param new_pass New password of the player
+     * @return A boolean if changed
+     */
     public boolean updatePlayerProfile(String name, String nickname, String email, String old_pass, String new_pass) {
         Player p1 = (Player) MainUI.session.getAttribute("user");
         p1 = dao.getPlayer(p1.getName());
@@ -79,6 +122,12 @@ public class PlayerController {
         return result;
     }
 
+    /**
+     * Check if a player has played a game
+     * 
+     * @param nickname Nickname of the player
+     * @return A boolean if the player has played
+     */
     public boolean ifPlayerPlay(String nickname) {
         boolean haJugado = false;
         Player p = dao.getPlayer(nickname);
